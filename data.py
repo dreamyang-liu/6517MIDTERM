d883cf96920f51f33cdaf6d23eaed649cf7c9f8c
Mengyang Liu <mliu444@gatech.edu>
2022-10-23 13:22:47 -0400

    Fixed dataset

--- data.py
+++ data.py
@@ -4,9 +4,12 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
-from torchvision.transforms import ToTensor, Compose, RandomResizedCrop, Resize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
+from torchvision.transforms import ToTensor, Compose, RandomResizedCrop, Normalize, RandomHorizontalFlip, RandomVerticalFlip, RandomRotation
+from config import *
 
-def get_task_data_in_numpy(dim=2, class_num=3):
+def get_task_data_in_numpy():
+    dim = DIM
+    class_num = CLASS_NUM
     BASE_DIR = 'downloads/' + f'task2_{dim}D_{class_num}'
     images = {
         'train': np.load(BASE_DIR +'classtrainimages.npy').astype(np.uint8),
@@ -18,28 +21,8 @@ def get_task_data_in_numpy(dim=2, class_num=3):
         'val': np.load(BASE_DIR +'classvallabels.npy').astype(np.uint8),
         'test': np.load(BASE_DIR +'classtestlabels.npy').astype(np.uint8)
     }
+    # breakpoint() # 410 24 45 2367
     return images, labels
-
-def get_task_data_in_torch(dim=2, class_num=3):
-    BASE_DIR = 'downloads/' + f'task2_{dim}D_{class_num}'
-    images = {
-        'train': np.load(BASE_DIR +'classtrainimages.npy').astype(np.float32),
-        'val': np.load(BASE_DIR +'classvalimages.npy').astype(np.float32),
-        'test': np.load(BASE_DIR +'classtestimages.npy').astype(np.float32)
-    }
-    labels = {
-        'train': np.load(BASE_DIR +'classtrainlabels.npy').astype(np.int64),
-        'val': np.load(BASE_DIR +'classvallabels.npy').astype(np.int64),
-        'test': np.load(BASE_DIR +'classtestlabels.npy').astype(np.int64)
-    }
-    images['train'] = torch.from_numpy(images['train']).float()
-    images['val'] = torch.from_numpy(images['val']).float()
-    images['test'] = torch.from_numpy(images['test']).float()
-    labels['train'] = torch.from_numpy(labels['train'])
-    labels['val'] = torch.from_numpy(labels['val'])
-    labels['test'] = torch.from_numpy(labels['test'])
-    return images, labels
-
 
 class NeuroDataset(Dataset):
     def __init__(self, stage='train', transform=None):
@@ -54,28 +37,29 @@ class NeuroDataset(Dataset):
 
     def __getitem__(self, idx):
         concat = self.concat[idx].transpose(1,2,0)
-        concat = Image.fromarray(concat)
         if self.transform:
             concat = self.transform(concat)
-        return concat[0, np.newaxis, :, :], (concat[1, np.newaxis, :, :] * 255).long()
+        return concat[0, np.newaxis, :, :], (concat[1, :, :] * 255).long()
 
 class NeuroDataModule(pl.LightningDataModule):
     RESIZE_SHAPE = [224, 224]
-    RESIZE_CROP_SHAPE = [64, 64]
+    RESIZE_CROP_SHAPE = [224, 224]
 
     def __init__(self, batch_size=32):
         super().__init__()
         self.batch_size = batch_size
-        transforms = Compose([
-            # RandomResizedCrop(self.RESIZE_CROP_SHAPE),
-            # RandomHorizontalFlip(),
-            # RandomVerticalFlip(),
-            # RandomRotation(90),
+        train_transforms = Compose([
+            ToTensor(),
+            RandomVerticalFlip(p=0.5),
+            RandomHorizontalFlip(p=0.5),
+            RandomRotation((0,180)),
+        ])
+        val_transforms = Compose([
             ToTensor(),
         ])
-        self.neuro_train = NeuroDataset(stage='train', transform=transforms)
-        self.neuro_val = NeuroDataset(stage='val', transform=transforms)
-        self.neuro_test = NeuroDataset(stage='test', transform=transforms)
+        self.neuro_train = NeuroDataset('train', transform=train_transforms)
+        self.neuro_val = NeuroDataset('val', transform=val_transforms)
+        self.neuro_test = NeuroDataset('test', transform=val_transforms)
 
     def setup(self, stage=None):
         pass
@@ -91,8 +75,4 @@ class NeuroDataModule(pl.LightningDataModule):
 
 
 if __name__ == "__main__":
-    c = NeuroDataModule()
-    for i in c.train_dataloader():
-        print(i[0].shape)
-        print(i[1].shape)
-        break
+    pass
--- utils.py
+++ utils.py
@@ -1,28 +1,66 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
 FIG_PATH = 'figs'
+os.makedirs(FIG_PATH, exist_ok=True)
+
 def draw_mask(img, mask):
     """Draws mask on an image.
     """
     mask = mask.astype(np.uint8)
+    img = np.copy(img)
     h, w = img.shape[:2]
     for i in range(h):
         for j in range(w):
             if mask[i, j] == 1:
-                img[i, j] = (0, 0, 255)
+                img[i, j] = np.array([247, 86, 124], dtype=np.float32) / 255
             elif mask[i, j] == 2:
-                img[i, j] = (0, 255, 0)
+                img[i, j] = np.array([153, 225, 217], dtype=np.float32) / 255
             elif mask[i, j] == 3:
-                img[i, j] = (255, 0, 0)
+                img[i, j] = np.array([238, 227, 171], dtype=np.float32) / 255
     return img
 
-def draw_mask_for_list(imgs, masks, masks_pred):
+def draw_mask_comparsion(img, mask, mask_pred):
     """Draws mask on a list of images.
     """
-    img = imgs.numpy()[0, 0]
-    mask = masks.numpy()[0, 0]
-    mask_pred = masks_pred.numpy()[0]
-    plt.figure()
-    plt.imshow((mask_pred * 50).astype(np.uint8), cmap='gray')
-    plt.savefig('mask_pred.png')
-    return None
+    h, w= img.shape
+    img = np.repeat(img.reshape(h, w, 1), 3, axis=2)
+    annotated = draw_mask(img, mask)
+    annotated_pred = draw_mask(img, mask_pred)
+    return (img, annotated, annotated_pred)
+
+def save_result(img, annotated, annotated_pred, filename):
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.imshow(img)
+    plt.title('Original')
+    plt.subplot(1, 3, 2)
+    plt.imshow(annotated)
+    plt.title('Ground Truth')
+    plt.subplot(1, 3, 3)
+    plt.imshow(annotated_pred)
+    plt.title('Prediction')
+    plt.savefig(filename)
+
+if __name__ == '__main__':
+    images = np.load('downloads/task2_2D_4classtestimages.npy')
+    masks = np.load('downloads/task2_2D_4classtestlabels.npy')
+    img = images[0]
+    mask = masks[0]
+    mask_pred = masks[0]
+    h, w= img.shape
+    img = np.repeat(img.reshape(h, w, 1), 3, axis=2)
+    annotated = draw_mask(img, mask)
+    annotated_pred = draw_mask(img, mask_pred)
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    plt.imshow(img)
+    plt.title('Original')
+    plt.subplot(1, 3, 2)
+    plt.imshow(annotated)
+    plt.title('Ground Truth')
+    plt.subplot(1, 3, 3)
+    plt.imshow(annotated_pred)
+    plt.title('Prediction')
+    plt.savefig('annotated.png')
